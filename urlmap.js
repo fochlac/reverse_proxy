549f6798d65dcc2eb80f482fb4fc511c5876a2ac
root <root@fochlac.com>
2017-06-04 01:24:51 +0000

    first commit

---
+++ urlmap.js
@@ -0,0 +1,41 @@
+module.exports = {
+    "crawler.fochlac.com": {
+        "url": "",
+        "port": "CRAWLER_PORT",
+        "redirect": false
+    },
+    "www.fochlac.com": {
+        "redirect": true,
+        "redirectUrl": "fochlac.com"
+    },
+    "fochlac.com": {
+        "url": "",
+        "port": "WWW_PORT",
+        "redirect": false
+    },
+    "home.fochlac.com": {
+        "url": "/Fochlac",
+        "port": "SP_PORT",
+        "redirect": false
+    },
+    "caro.fochlac.com": {
+        "url": "/caro",
+        "port": "SP_PORT",
+        "redirect": false
+    },
+    "fg.fochlac.com": {
+        "url": "/fg",
+        "port": "SP_PORT",
+        "redirect": false
+    },
+    "dsa.fochlac.com": {
+        "url": "",
+        "port": "DSA_PORT",
+        "redirect": false
+    },
+    "mongo.fochlac.com": {
+        "url": "",
+        "port": "MONGO_PORT",
+        "redirect": false
+    }
+};
